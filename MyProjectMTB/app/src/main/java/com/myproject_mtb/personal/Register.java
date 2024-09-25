c2f7edb47defe186036032dddcd04f53774c9cba
Thomas <Tserna1@cesde.net>
2024-09-24 21:11:57 -0500

    Clase 24 Septiembre
    Envio registro a Firebase

--- MyProjectMTB/app/src/main/java/com/myproject_mtb/personal/Register.java
+++ MyProjectMTB/app/src/main/java/com/myproject_mtb/personal/Register.java
@@ -6,10 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    DatabaseReference reference = database.getReference().child("Usuario");
+
     Button btnVolverHome;
+
+    //Enlazar los input de registro con la BD de Firebase
+    EditText inputId;
+    EditText inputUserName;
+    EditText inputLastName;
+    EditText inputPhone;
+    EditText inputEmail;
+    EditText inputPassword;
+    Button btnSendUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,22 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         btnVolverHome = findViewById(R.id.btn_volver_home);
+        inputId = findViewById(R.id.input_user_id);
+        inputUserName = findViewById(R.id.input_user_name);
+        inputLastName = findViewById(R.id.input_user_last_name);
+        inputPhone = findViewById(R.id.input_user_phone);
+        inputEmail = findViewById(R.id.input_user_mail);
+        inputPassword = findViewById(R.id.input_user_password);
+        btnSendUser = findViewById(R.id.btn_register);
+
+        //Para llamar la funcion del boton de registro
+        btnSendUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
 
         btnVolverHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,12 +59,36 @@ public class Register extends AppCompatActivity {
                 volverHomeDesdeRegistro();
             }
         });
-
     }
 
     public void volverHomeDesdeRegistro(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+    }
+
+    public void createUser(){
+
+        String userId = inputId.getText().toString();
+        DatabaseReference newUserRef = reference.child(String.valueOf(userId));
+
+        String name = inputUserName.getText().toString();
+        newUserRef.child("Name").setValue(name);
+
+        String lastName = inputLastName.getText().toString();
+        newUserRef.child("LastName").setValue(lastName);
+
+        String phone = inputPhone.getText().toString();
+        newUserRef.child("Phone").setValue(phone);
+
+        String email = inputEmail.getText().toString();
+        newUserRef.child("Email").setValue(email);
+
+        String password = inputPassword.getText().toString();
+        newUserRef.child("Password").setValue(password);
+
+        //Para que muestre un mensaje de registro exitoso
+        Toast.makeText(getApplicationContext(), "Usuario registrado exitosamente", Toast.LENGTH_LONG).show();
+
     }
 
 }
